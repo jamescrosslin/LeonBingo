6218f3a4252ef649b31bc51c51ceff207d42cd8c
James Crosslin <jamesmscrosslin@gmail.com>
2020-11-23 21:32:00 -0800

    Tackled some clipping animation and added color
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 const squares = document.querySelectorAll("#board section")
+const board = document.getElementById("board")
+
+board.addEventListener("click", (event) => {
+  event.target.className = "stamp"
+})
