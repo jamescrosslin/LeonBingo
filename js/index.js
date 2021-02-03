bcca2166c5780ff9d8e3b02ec0230c62707c7e01
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-03 14:34:12 -0800

    removed arctext library

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-const squares = document.querySelectorAll("#board section")
-const board = document.getElementById("board")
+const squares = document.querySelectorAll("#board section");
+const board = document.getElementById("board");
+const shuffleBtn = document.getElementById("shuffleBtn");
+const coverAllBtn = document.getElementById("coverAllBtn");
+let coverAllMode = false;
 
 const phrases = [
   "Leon acknowledges a hair compliment",
@@ -46,28 +49,77 @@ const phrases = [
   "What should go in the head tag?",
   "anchor tags are for links ⚓",
   "Progressive Enhancement"
-]
+];
 
-shuffleArray(phrases)
+function populateBoard() {
+  shuffleArray(phrases);
 
-squares.forEach((square, i) => {
-  if (i !== 12) {
-    square.innerHTML = phrases[i]
-  }
-  i++
-})
+  squares.forEach((square, i) => {
+    if (i !== 12) {
+      square.innerHTML = phrases[i];
+    }
+    i++;
+  });
+}
+
+const winScenario = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24],
+  [0, 5, 10, 15, 20],
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 13, 18, 23],
+  [4, 9, 14, 19, 24],
+  [0, 6, 12, 18, 24],
+  [4, 8, 12, 16, 20],
+  [0, 4, 12, 20, 24]
+];
 
 function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1))
-    var temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+}
+
+function winCheck() {
+  winScenario.forEach((scenario) => {
+    if (coverAllMode) {
+      const stampedSquares = Array.from(squares).filter((square) =>
+        square.classList.contains("stamp")
+      );
+      stampedSquares.length === 25 && board.remove();
+    } else {
+      let count = 0;
+      scenario.forEach((num) => {
+        if (squares[num].classList.contains("stamp")) {
+          count++;
+        }
+      });
+      if (count === 5) board.remove();
+    }
+  });
 }
 
 board.addEventListener("click", (event) => {
   if (event.target.id !== "freeSpace") {
-    event.target.classList.toggle("stamp")
+    event.target.classList.toggle("stamp");
+    winCheck();
   }
-})
+});
+
+shuffleBtn.addEventListener("click", populateBoard);
+
+coverAllBtn.addEventListener("click", (event) => {
+  coverAllMode = !coverAllMode;
+  event.target.innerHTML =
+    event.target.innerHTML === `Cover All Mode: Off`
+      ? `Cover All Mode: On`
+      : `Cover All Mode: Off`;
+  setTimeout(winScenario, 600);
+});
+
+populateBoard();
